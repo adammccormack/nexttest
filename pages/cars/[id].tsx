53e670e7ff962a8b1d29d138c44c4e444c4bfc45
amccormack <79875785+adammccormack@users.noreply.github.com>
2022-12-17 11:39:04 +0000

    updated dynamic car page

--- pages/cars/[id].tsx
+++ pages/cars/[id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Car({ car }) {
     const router = useRouter()
@@ -7,31 +7,36 @@ export default function Car({ car }) {
     
     // render the id value out to the template.
     return (<>
-        <Head>
-            <title>{car.color} {car.id}</title>
-        </Head>
-        <h1>{id}</h1> 
+    <Head>
+        <title>{car.color}</title>
+    </Head>
+        <h1>{id}</h1>
         <img src={car.image} />
     </>)
+}
+
+
+export async function getServerSideProps({ params }) {
+    
 }
 
 export async function getStaticProps({ params }) {
 
     const req = await fetch(`http://localhost:3000/${params.id}.json`);
     const data = await req.json();
-
+    // when getStaticProps is inside component, auto sends props to Component, i.e. Car({ car })
     return {
         props: { car: data },
     }
 }
 
 export async function getStaticPaths() {
-
-    const req = await fetch('http://localhost:3000/cars.json')
+     
+    const req = await fetch('http://localhost:3000/cars.json');
     const data = await req.json();
-
-    const paths = data.map( car => {
-        return { params: { id: car }}
+    
+    const paths = data.map(car => { // .map makes path object contain array. 
+        return { params: { id: car} }
     })
 
     return {
