e65f6d9216197a57b7a87cd5b5ecd90efc177ee2
amccormack <79875785+adammccormack@users.noreply.github.com>
2022-12-19 00:33:38 +0000

    added getServerSideProps function

--- pages/cars/[id].tsx
+++ pages/cars/[id].tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 export default function Car({ car }) {
     const router = useRouter()
     const { id } = router.query // when the user queries on the url, return that query as id.
-    
+
     // render the id value out to the template.
     return (<>
     <Head>
@@ -15,32 +15,36 @@ export default function Car({ car }) {
     </>)
 }
 
-
 export async function getServerSideProps({ params }) {
-    
-}
-
-export async function getStaticProps({ params }) {
-
     const req = await fetch(`http://localhost:3000/${params.id}.json`);
     const data = await req.json();
-    // when getStaticProps is inside component, auto sends props to Component, i.e. Car({ car })
+
     return {
         props: { car: data },
     }
 }
 
-export async function getStaticPaths() {
-     
-    const req = await fetch('http://localhost:3000/cars.json');
-    const data = await req.json();
-    
-    const paths = data.map(car => { // .map makes path object contain array. 
-        return { params: { id: car} }
-    })
+// export async function getStaticProps({ params }) {
 
-    return {
-        paths,
-        fallback: false
-    };
-}
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+//     // when getStaticProps is inside component, auto sends props to Component, i.e. Car({ car })
+//     return {
+//         props: { car: data },
+//     }
+// }
+
+// export async function getStaticPaths() {
+
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
+    
+//     const paths = data.map(car => { // .map makes path object contain array. 
+//         return { params: { id: car} }
+//     })
+
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// }
